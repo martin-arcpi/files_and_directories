4bc505966c292975573c70dfb93917e21ec86072
martin-arcpi <104268393+martin-arcpi@users.noreply.github.com>
2026-03-24 14:42:23 +0000

    Added file size recording capabiliy.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
+import csv
 
-print(os.listdir())
+
+# TODO add functionality to save file names in a csv file with other details such as file size
 
 with open("files.txt", "w") as f:
     for file_name in os.listdir():
-        f.write(f"{file_name}\n")
+        f.write(f"{file_name}  |    {os.path.getsize(file_name)}\n")
 
 
 
